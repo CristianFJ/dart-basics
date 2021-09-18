1f34254e6d8ab081f5fb410336ebc5b8cb9872ac
Cristian <36268866+CristianFJ@users.noreply.github.com>
2021-09-17 22:07:48 -0500

    Create 3.bool-condicion.dart

---
+++ 3.bool-condicion.dart
@@ -0,0 +1,8 @@
+void main() {
+  bool? isActive = null;//permite asignar null a isActive
+  if ( isActive == null ) {
+    print( 'isActive es null' );
+  } else {
+    print( 'No es null' );
+  } 
+}
