05d105a45110bbe2a465368fedaac28f1fa1bf18
Cristian <36268866+CristianFJ@users.noreply.github.com>
2021-09-18 15:13:17 -0500

    Create 6_Funciones.dart

---
+++ 6_Funciones.dart
@@ -0,0 +1,21 @@
+  
+void main() {
+  
+  final nombre = 'Cristian';
+  
+//   saludar( nombre, 'Greetings' );
+  saludar2( nombre: nombre, mensaje: 'Greetings' );
+  
+}
+
+void saludar( String nombre, [ String mensaje = 'Hi' ]) {
+  print('$mensaje $nombre');
+}
+
+
+void saludar2({ 
+  required String nombre, 
+  required String mensaje,
+ }) {
+  print('$mensaje $nombre');
+}
